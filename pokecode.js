76e0dc645acb87de55d9d64e4f5f920a3cf5faa1
jorge <jorgeantoniosantos27@gmail.com>
2024-02-17 02:26:20 -0300

    removing comments

--- pokecode.js
+++ pokecode.js
@@ -93,34 +93,5 @@ btnSprites.addEventListener("click",()=>{
 
 
 
-// const btn= document.getElementById("botao")
-// btn.addEventListener("click",
-// async function fetchData(){
-
-//     try{
-
-//         const pokemonName = document.querySelector("#pokemonName").value.toLowerCase();
-//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-//         if(!response.ok){
-//             throw new Error("Could not fetch resource");
-//         }
-
-//         const data = await response.json();
-//         const pokemonSprite = data.sprites.front_default;
-//         const imgElement = document.getElementById("pokemonSprite");
-
-//         imgElement.src = pokemonSprite;
-//         imgElement.style.display = "block";
-       
-//     }
-//     catch(error){
-//         console.error(error);
-//     }
-// }
-
-// )
-
-
 
 
