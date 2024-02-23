1ee0548ef6ddf7595065abaa10006f40a5550b61
jorge <jorgeantoniosantos27@gmail.com>
2024-02-23 18:38:52 -0300

    becoming responsive

--- pokecode.js
+++ pokecode.js
@@ -1,41 +1,12 @@
 
 const pokemonName = document.querySelector("#pokemonName").value.toLowerCase();
-const btnSprites=document.querySelector('.btn-sprites');
+
 const btnAll=document.querySelector('.btn-all');
-
-btnSprites.addEventListener("click",()=>{
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    .then(data=>{
-        return data.json()
-    })
-    .then(data=>{
-        
-            
-       
-        const insert=`<div class="pokemon-template">
-        <h2 class="pokemon-template-title"></h2>
-        <div class="pokemon-info">
-            <p>pokemon type</p>
-            <img src="" alt="Pokemon Sprite" id="pokemonSprite" style="display: none">
-        </div>` 
-       
-    })
-})
-
-// fetch(`https://pokeapi.co/api/v2/pokemon/ditto`).then(data=>{
-    
-       
-   
-    // return data.json(); })
-    // .then(res=>{
-    //     test= res.species.name;
-    //     console.log(test);
-    // })
 
 
     btnAll.addEventListener("click", () => {
         const pokemonPromises = [];
-        for (let i = 152; i <= 251; i++) {
+        for (let i = 1; i <= 300; i++) {
           pokemonPromises.push(
             fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
               .then(data => {
